c4670fc7fbc83d2a9562cc793998759beecab0d1
Franky Viray <harrypotterfan6969@yahoo.com>
2020-10-14 15:24:49 -0700

    worked on header and footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+            <container>
+                <row>
+                    <Col className='text-center py-3'>
+                        Copyright &copy; Proshop
+                    </Col>
+                </row>
+            </container>
+        </footer>
+    )
+}
+
+export default Footer
