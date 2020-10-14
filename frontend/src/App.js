2f149532726ec6059ed104acff600ad46a66e2ea
Franky Viray <harrypotterfan6969@yahoo.com>
2020-10-14 15:59:18 -0700

    left off on video 6 added the sample items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <container>
-          <h1>Welcome to Pro Shop</h1>
+          <HomeScreen />
         </container>
       </main>
       <Footer />
