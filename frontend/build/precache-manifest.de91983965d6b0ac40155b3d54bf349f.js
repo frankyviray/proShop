83cf84e1b78ace36d3a03406e5f82c1aff2502b7
Franky Viray <harrypotterfan6969@yahoo.com>
2020-10-16 09:49:35 -0700

    added the amount and rating

---
+++ frontend/build/precache-manifest.de91983965d6b0ac40155b3d54bf349f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0bc1275e02a3ef11ae622320ef90a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d03a77962edbc3d81b38",
+    "url": "/static/css/main.d87522dd.chunk.css"
+  },
+  {
+    "revision": "534668f39a48ca1816b9",
+    "url": "/static/js/2.d5d739cb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d5d739cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03a77962edbc3d81b38",
+    "url": "/static/js/main.f560e805.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
